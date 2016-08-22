54c69763b2729e8ecc3284270cf7ead0a46063ab
Thomas Borghs <tborghs@gmail.com>
2016-08-22 21:30:43 +0200

    converted script to rip audio only

--- rip_media.sh
+++ rip_media.sh
@@ -1,27 +1,9 @@
 #!/bin/bash
 
-MEDIA=
-RIP_LOG_LOCATION="/var/log/rip/rip.log"
-
 sleep 2
-( set -o posix ; set ) >> "/var/log/rip/rip_posix_debug.log"
-cd /home/knuppel/rip
 
-if [ "$ID_CDROM_MEDIA_BD" = "1" ]
+if [ "$ID_CDROM_MEDIA_CD" = "1" ]
 then
-        MEDIA=bluray
-        (
-        echo "$MEDIA" >> /var/log/rip/DiscTypeTest.log
-        ) &
-elif [ "$ID_CDROM_MEDIA_DVD" = "1" ]
-then
-        MEDIA=dvd
-        (
-        echo "$MEDIA" >> /var/log/rip/DiscTypeTest.log
-        ) &
-elif [ "$ID_CDROM_MEDIA_CD" = "1" ]
-then
-	echo "ripping audio cd" >> "$RIP_LOG_LOCATION"
 	echo "sudo -u knuppel /home/knuppel/workspace/auto-rip-script/rip_audio_cd.sh" | at now
 fi
 exit 0
